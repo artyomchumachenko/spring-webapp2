9ecb0d109da08373d32eaebde429b637c270643e
artyomchumachenko <artyomchumachenko@mail.ru>
2022-06-05 13:46:08 +0300

    adding a list of messages and their tags to the database; adding a filter to the list of messages by tags

--- src/main/java/com/example/springwebapp2/controller/GreetingController.java
+++ src/main/java/com/example/springwebapp2/controller/GreetingController.java
@@ -1,13 +1,23 @@
 package com.example.springwebapp2.controller;
 
+import com.example.springwebapp2.domain.Message;
+import com.example.springwebapp2.repos.MessageRepo;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.List;
 import java.util.Map;
 
 @Controller
 public class GreetingController {
+    private final MessageRepo messagesRepo;
+
+    public GreetingController(MessageRepo messagesRepo) {
+        this.messagesRepo = messagesRepo;
+    }
+
     @GetMapping("/greeting")
     public String greeting(
             @RequestParam(name = "name", required = false, defaultValue = "World") String name,
@@ -19,7 +29,39 @@ public class GreetingController {
 
     @GetMapping
     public String main(Map<String, Object> model) {
-        model.put("some", "hello, Artyom!");
+        Iterable<Message> messages = messagesRepo.findAll();
+
+        model.put("messages", messages);
+        return "main";
+    }
+
+    @PostMapping
+    public String add(
+            @RequestParam String text,
+            @RequestParam String tag,
+            Map<String, Object> model
+    ) {
+        Message message = new Message(text, tag);
+        messagesRepo.save(message);
+
+        Iterable<Message> messages = messagesRepo.findAll();
+        model.put("messages", messages);
+
+        return "main";
+    }
+
+    @PostMapping("/filter")
+    public String filter(
+            @RequestParam String filter,
+            Map<String, Object> model
+    ) {
+        Iterable<Message> messages;
+        if (filter != null && !filter.isEmpty()) {
+            messages = messagesRepo.findByTag(filter);
+        } else {
+            messages = messagesRepo.findAll();
+        }
+        model.put("messages", messages);
         return "main";
     }
 }
---
+++ src/main/java/com/example/springwebapp2/repos/MessageRepo.java
@@ -0,0 +1,10 @@
+package com.example.springwebapp2.repos;
+
+import com.example.springwebapp2.domain.Message;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface MessageRepo extends CrudRepository<Message, Long> {
+    List<Message> findByTag(String tag);
+}
